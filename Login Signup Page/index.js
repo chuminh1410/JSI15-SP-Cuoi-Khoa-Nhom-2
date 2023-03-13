cdbdff36b4cddae8d6ad3ec24af10d9913fa2710
chuminh1410 <chungocquangminh@gmail.com>
2023-03-13 16:10:41 +0700

    Up login signup Lần 1

--- Login Signup Page/index.js
+++ Login Signup Page/index.js
@@ -1,64 +1,35 @@
-class User {
-    constructor(name, email, password) {
-        this.name = name;
-        this.email = email;
-        this.password = password;
-    }
+const signInContainer = document.querySelector('.container');
+const signUpContainer = document.querySelector('.sign-up-container');
+
+function showSignUp() {
+    signInContainer.style.display = 'none';
+    signUpContainer.style.display = 'flex';
 }
 
-const signUpBtn = document.getElementById("sign-up-btn");
-const signInBtn = document.getElementById("sign-in-btn");
-const signUpLink = document.getElementById("sign-up-link");
-const signInLink = document.getElementById("sign-in-link");
-const signUpContainer = document.querySelector(".sign-up-container");
-const signInContainer = document.querySelector(".sign-in-container");
+function showSignIn() {
+    signUpContainer.style.display = 'none';
+    signInContainer.style.display = 'flex';
+}
 
-let users = [];
+document.getElementById('submit-btn').addEventListener('click', function(event) {
+    event.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
 
-signUpBtn.addEventListener("click", e => {
-    e.preventDefault();
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-    const confirmPassword = document.getElementById("confirm-password").value;
-    if (password === confirmPassword) {
-        const user = new User(name, email, password);
-        users.push(user);
-        localStorage.setItem("users", JSON.stringify(users));
-        alert("Sign up successful!");
-    } else {
-        alert("Passwords do not match.");
-    }
+    // Here you can add the logic to authenticate the user
+
+    alert(`User ${email} signed in successfully.`);
 });
 
-signInBtn.addEventListener("click", e => {
-    e.preventDefault();
-    const signInEmail = document.getElementById("sign-in-email").value;
-    const signInPassword = document.getElementById("sign-in-password").value;
-    const user = users.find(
-        user => user.email === signInEmail && user.password === signInPassword
-    );
-    if (user) {
-        alert(`Welcome back, ${user.name}!`);
-    } else {
-        alert("Incorrect email or password.");
-    }
-});
+document.getElementById('signup-btn').addEventListener('click', function(event) {
+    event.preventDefault();
+    const email = document.getElementById('new-email').value;
+    const password = document.getElementById('new-password').value;
 
-signInLink.addEventListener("click", e => {
-    e.preventDefault();
-    signInContainer.style.display = "block";
-    signUpContainer.style.display = "none";
-});
+    // Here you can add the logic to store the user details in local storage
+    
+    localStorage.setItem('user_email', email);
+    localStorage.setItem('user_password', password);
 
-signUpLink.addEventListener("click", e => {
-    e.preventDefault();
-    signUpContainer.style.display = "block";
-    signInContainer.style.display = "none";
-});
-
-window.addEventListener("load", () => {
-    if (localStorage.getItem("users")) {
-        users = JSON.parse(localStorage.getItem("users"));
-    }
+    alert(`User ${email} signed up successfully.`);
 });
